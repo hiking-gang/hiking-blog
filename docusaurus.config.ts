6c787bca95498111559e2f9622e97813512cc719
marius <156966784+hiking-gang@users.noreply.github.com>
2024-08-09 21:31:16 +0200

    update structure
    remove docs
    
    add other topics

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,20 +3,16 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Hiking Blog',
-  tagline: 'Dinosaurs are cool',
+  title: 'Hiking',
+  tagline: '... but not only',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://hiking-gang.github.io',
+
   baseUrl: '/hiking-blog/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'hiking-gang', // Usually your GitHub org/user name.
-  projectName: 'https://github.com/hiking-gang/hiking-blog.git', // Usually your repo name.
+  organizationName: 'hiking-gang',
+  projectName: 'https://github.com/hiking-gang/hiking-blog.git',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -33,19 +29,9 @@ const config: Config = {
     [
       'classic',
       {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        docs: false,
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -55,7 +41,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'Hiking',

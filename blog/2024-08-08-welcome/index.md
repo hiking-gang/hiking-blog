b5915ab8617f83f3a1c5a130f5c482d14b4458fa
marius <156966784+hiking-gang@users.noreply.github.com>
2024-08-09 08:19:52 +0200

    adjust add initial blogposts

---
+++ blog/2024-08-08-welcome/index.md
@@ -0,0 +1,9 @@
+---
+slug: welcome
+title: Welcome
+authors: [hiking-gang]
+tags: [hiking]
+---
+
+You will soon get to know more about this blog. 🙃
+
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,69 +58,17 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'Hiking Blog',
+      title: 'Hiking',
       logo: {
         alt: 'Hiking Blog Logo',
-        src: 'img/logo.svg',
+        src: 'img/mountains.png',
       },
       items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
         {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
       ],
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
       copyright: `Copyright © ${new Date().getFullYear()} Hiking Blog, Built with Docusaurus 🦖.`,
     },
     prism: {
